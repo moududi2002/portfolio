b44d9572edf01ecf673a9010c2fd6ffabd69c96f
Abul Ala Moududi <moududi2002@gmail.com>
2025-10-30 12:37:22 +0600

    .next change

--- next.config.js
+++ next.config.js
@@ -1,13 +1,1 @@
-module.exports = {
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'moududi.researchustad.org' }],
-        destination: 'https://moududi.researchustad.org/:path*',
-        permanent: true,
-      },
-    ];
-  },
-};
+
